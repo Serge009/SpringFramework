1cdbf78ba92cc5ba9d83854ab5b997a6def85197
Serge009 <serge.naumovych@gmail.com>
2014-07-22 17:54:45 +0300

    10

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,4 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.PropertyResolver;
 
 import java.util.List;
@@ -5,26 +6,12 @@ import java.util.List;
 /**
  * Created by Matrix on 22.07.2014.
  */
+
 public class Triangle {
 
-    private String type;
-
-    private int height;
-
-    private List<Point> points;
-
-    public List<Point> getPoints() {
-        return points;
-    }
-
-    public void setPoints(List<Point> points) {
-        this.points = points;
-    }
-    /*
     private Point pointA;
     private Point pointB;
     private Point pointC;
-
 
     public Point getPointA() {
         return pointA;
@@ -49,31 +36,13 @@ public class Triangle {
     public void setPointC(Point pointC) {
         this.pointC = pointC;
     }
-    */
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
 
     @Override
     public String toString() {
         return "Triangle{" +
-                "type='" + type + '\'' +
-                ", height=" + height +
-                ", points=" + points +
+                "pointA=" + pointA +
+                ", pointB=" + pointB +
+                ", pointC=" + pointC +
                 '}';
     }
 
