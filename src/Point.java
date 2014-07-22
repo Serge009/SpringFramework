62ef6b31b0d1d34b44c49eb63cd7353e083cf309
Serge009 <serge.naumovych@gmail.com>
2014-07-22 16:14:56 +0300

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Matrix on 22.07.2014.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
